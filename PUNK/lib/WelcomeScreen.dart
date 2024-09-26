5cecb53650993ca6b5a76513e8d03f75ee7a9366
KABOPOK <mishavvvsweater@gmail.com>
2024-09-26 20:06:05 +0300

    7

---
+++ PUNK/lib/WelcomeScreen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'RegistrationForm.dart';
+import 'LoginForm.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildTitle(),
+            SizedBox(height: 50),
+            _buildLoginButton(context),
+            SizedBox(height: 21),
+            _buildRegistrationButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Column(
+      children: [
+        Text(
+          'ПУНК',
+          style: TextStyle(
+            color: Colors.orange,
+            fontSize: 80,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'market',
+          style: TextStyle(
+            color: Colors.orange,
+            fontSize: 50,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginForm()),
+        );
+      },
+      child: Text(
+        'ВОЙТИ',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+
+  Widget _buildRegistrationButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => RegistrationForm()),
+        );
+      },
+      child: Text(
+        'РЕГИСТРАЦИЯ',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}

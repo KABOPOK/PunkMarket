1b68c36cc3665c42741d641e246c2dfca2357236
KABOPOK <mishavvvsweater@gmail.com>
2024-10-18 00:13:19 +0300

    remove abligatory photo upload

--- server/app/routes/registration_routes.py
+++ server/app/routes/registration_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy import nullsfirst
 from werkzeug.utils import secure_filename
 import os
 from ..db import connect_db
@@ -16,37 +17,37 @@ def create_user():
     user_name = data.get('userName')
     location = data.get('location')
     telegramID = data.get('telegramID')
-
+    image = nullsfirst
     if 'image' not in request.files:
-        return jsonify({"error": "No image file in the request"}), 400
+        image = 'null'
+        photoUrl = 'null'
+    else:
+        image = request.files['image']
 
-    image = request.files['image']
-
-    if image and allowed_file(image.filename):
+    if image and image != 'null' and allowed_file(image.filename):
         filename = secure_filename(image.filename)
         image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         image.save(image_path)
-
         photoUrl = f"http://192.168.50.20:5000/uploads/{filename}"
 
-        conn = connect_db()
-        if conn is None:
-            return jsonify({"error": "Database connection failed"}), 500
+    conn = connect_db()
+    if conn is None:
+        return jsonify({"error": "Database connection failed"}), 500
 
-        cursor = conn.cursor()
-        try:
-            cursor.execute(
-                "INSERT INTO users (userID, number,password,  username, photoUrl, location, telegramID ) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                (userID, number, password,user_name, photoUrl, location, telegramID)
-            )
-            conn.commit()
-        except Exception as e:
-            conn.rollback()
-            return jsonify({"error": str(e)}), 400
-        finally:
-            cursor.close()
-            conn.close()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            "INSERT INTO users (userID, number,password,  username, photoUrl, location, telegramID ) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (userID, number, password, user_name, photoUrl, location, telegramID)
+        )
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": str(e)}), 400
+    finally:
+        cursor.close()
+        conn.close()
 
-        return jsonify({"message": "User created successfully!", "photoUrl": photoUrl})
+    return jsonify({"message": "User created successfully!", "photoUrl": photoUrl})
 
     return jsonify({"error": "Invalid image format"}), 400

d822c2bdd2b2c62745fbc274a7b4a9e975a5ae10
KABOPOK <mishavvvsweater@gmail.com>
2024-11-12 14:57:29 +0300

    set images transfering

--- server/src/main/java/kabopok/server/controllers/ProductController.java
+++ server/src/main/java/kabopok/server/controllers/ProductController.java
@@ -3,9 +3,12 @@ package kabopok.server.controllers;
 import generated.kabopok.server.api.ProductApi;
 import generated.kabopok.server.api.model.ProductDTO;
 import kabopok.server.entities.Product;
+import kabopok.server.entities.User;
 import kabopok.server.mappers.ProductMapper;
 import kabopok.server.minio.StorageService;
+import kabopok.server.repositories.UserRepository;
 import kabopok.server.services.ProductService;
+import kabopok.server.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +27,7 @@ public class ProductController implements ProductApi {
   private final ProductMapper productMapper;
   private final StorageService storageService;
 
+  private final UserService userService;
   @Override
   public void createProduct(ProductDTO productDTO, List<MultipartFile> images) {
     Product product = productMapper.map(productDTO);
@@ -38,10 +43,25 @@ public class ProductController implements ProductApi {
   }
 
   @Override
+  public List<ProductDTO> getMyProducts(UUID userId, Integer page, Integer limit) {
+    User user = userService.findById(userId);
+    List<Product> productList = productService.getMyProducts(userId,page,limit);
+    List<ProductDTO> productDTOList = new ArrayList<>();
+    productList.forEach(product -> {
+      String url = storageService.generateImageUrl("photos", product.getUserID().toString() + "/" + "envelop.jpg",3600);
+      product.setPhotoUrl(url);
+      productDTOList.add(productMapper.map(product));
+    });
+    return productDTOList;
+  }
+
+  @Override
   public List<ProductDTO> getProducts(Integer page, Integer limit) {
     List<Product> productList = productService.getProducts(page, limit);
     List<ProductDTO> productDTOList = new ArrayList<>();
     productList.forEach(product -> {
+      String url = storageService.generateImageUrl("photos", product.getUserID().toString() + "/" + "envelop.jpg",3600);
+      product.setPhotoUrl(url);
       productDTOList.add(productMapper.map(product));
     });
     return productDTOList;
--- server/src/main/java/kabopok/server/services/ProductService.java
+++ server/src/main/java/kabopok/server/services/ProductService.java
@@ -29,5 +29,9 @@ public class ProductService {
     Pageable pageable = PageRequest.of(page - 1, limit); // Page number is zero-based in Pageable
     return productRepository.findAll(pageable).getContent();
   }
+  public List<Product> getMyProducts(UUID userId, Integer page, Integer limit) {
+    Pageable pageable = PageRequest.of(page - 1, limit); // Page number is zero-based in Pageable
+    return productRepository.findAllByUserID(userId, pageable);
+  }
 
 }

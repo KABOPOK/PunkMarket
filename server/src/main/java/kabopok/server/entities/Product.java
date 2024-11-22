9133ad3278092da0a7681d192a89de8e9390e9cc
KABOPOK <mishavvvsweater@gmail.com>
2024-11-22 23:13:03 +0300

    set orm relation between product and user

--- server/src/main/java/kabopok/server/entities/Product.java
+++ server/src/main/java/kabopok/server/entities/Product.java
@@ -3,7 +3,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
 
 @Entity
 @Table(name = "products")
@@ -14,7 +18,7 @@ public class Product {
 
   @Id
   @Column(name = "product_id", columnDefinition = "UUID")
-  private UUID productID; // UUID for unique identification
+  private UUID productID;
 
   @Column(name = "price", nullable = false)
   private String price;
@@ -40,5 +44,8 @@ public class Product {
   @ManyToOne
   @JoinColumn(name = "user_id", referencedColumnName = "user_id")
   private User user;
-}
 
+//  @ManyToMany(mappedBy = "productsWish")
+//  private Set<User> usersWishedBy = new HashSet<>();
+
+}
--- server/src/main/java/kabopok/server/entities/User.java
+++ server/src/main/java/kabopok/server/entities/User.java
@@ -2,7 +2,11 @@ package kabopok.server.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -10,7 +14,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -44,4 +50,12 @@ public class User {
 
   @OneToMany(mappedBy = "user")
   private List<Product> products;
+
+//  @ManyToMany
+//  @JoinTable(
+//          name = "users_products",
+//          joinColumns = {@JoinColumn(name = "user_id")},
+//          inverseJoinColumns =  {@JoinColumn(name = "product_id")}
+//  )
+//  Set<Product> productsWish = new HashSet<>();
 }

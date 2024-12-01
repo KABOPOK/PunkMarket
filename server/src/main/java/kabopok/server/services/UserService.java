f53ff0b791b476bad420adc0b97dee3d43cee2a1
KABOPOK <mishavvvsweater@gmail.com>
2024-12-01 22:44:27 +0300

    remove double addProductToWishlist

--- server/src/main/java/kabopok/server/services/UserService.java
+++ server/src/main/java/kabopok/server/services/UserService.java
@@ -1,24 +1,13 @@
 package kabopok.server.services;
 
 import generated.kabopok.server.api.model.LoginDataDTO;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.errors.*;
 import kabopok.server.entities.Product;
 import kabopok.server.entities.User;
 import kabopok.server.repositories.ProductRepository;
 import kabopok.server.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -27,8 +16,6 @@ public class UserService extends DefaultService {
 
   private final UserRepository userRepository;
   private final ProductRepository productRepository;
-  private final JdbcTemplate jdbcTemplate;
-  private final MinioClient minioClient;
   public UUID save(User user) {
     UUID userID = UUID.randomUUID();
     user.setUserID(userID);
@@ -72,7 +59,6 @@ public class UserService extends DefaultService {
       heartState = true;
       user.getProductsWish().add(product);
     }
-    user.getProductsWish().add(product);
     userRepository.save(user);
     return heartState;
   }

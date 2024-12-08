a8697cc329d669eaae6101b9a45a07d8ceb832d7
Isabel Panova <panova.isabel@gmail.com>
2024-12-08 13:40:57 +0300

    feature/52 build: product tests are created (#56)
    * feature/52 build: product tests are created
    
    * Delete server/.idea directory
    
    * 52 move functions in separate class

--- server/src/test/java/kabopok/server/HttpSteps.java
+++ server/src/test/java/kabopok/server/HttpSteps.java
@@ -4,11 +4,17 @@ import generated.kabopok.server.api.model.LoginDataDTO;
 import generated.kabopok.server.api.model.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.io.Resource;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import java.util.List;
 
 @Component
 public class HttpSteps {
@@ -22,6 +28,20 @@ public class HttpSteps {
     MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
     body.add("user", userDTO);
     body.add("image", resource);
+    HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(body, headers);
+    return testRestTemplate.exchange(
+            url,
+            HttpMethod.POST,
+            request,
+            responseType
+    );
+  }
+  public <T> ResponseEntity<T> sendMultipartPostRequest(Object productDTO, List<Resource> resources, String url, Class<T> responseType) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+    body.add("product", productDTO);
+    resources.forEach(resource -> body.add("images", resource));
     HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(body, headers);
     return testRestTemplate.exchange(
             url,
@@ -46,6 +66,21 @@ public class HttpSteps {
     );
   }
 
+  public <T> ResponseEntity<T> sendMultipartPutRequest(Object productDTO, List<Resource> resources, String url, Class<T> responseType){
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+    body.add("product", productDTO);
+    resources.forEach(resource -> body.add("images", resource));
+    HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(body, headers);
+    return testRestTemplate.exchange(
+            url,
+            HttpMethod.PUT,
+            request,
+            responseType
+    );
+  }
+
   public ResponseEntity<Void> sendDeleteRequest(String url) {
     return testRestTemplate.exchange(
             url,
@@ -60,6 +95,18 @@ public class HttpSteps {
     headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity<LoginDataDTO> request = new HttpEntity<>(loginData, headers);
     return testRestTemplate.postForEntity(url, request, UserDTO.class);
+  }
+
+  public <T> ResponseEntity<List<T>> sendGetRequest(String url, ParameterizedTypeReference<List<T>> responseType) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<Object> request = new HttpEntity<>(headers);
+    return testRestTemplate.exchange(
+            url,
+            HttpMethod.GET,
+            request,
+            responseType
+    );
   }
 
 }
---
+++ server/src/test/java/kabopok/server/ProductTest.java
@@ -0,0 +1,181 @@
+package kabopok.server;
+
+import generated.kabopok.server.api.model.ProductDTO;
+import kabopok.server.entities.Product;
+import kabopok.server.entities.User;
+import kabopok.server.mappers.ProductMapper;
+import kabopok.server.repositories.ProductRepository;
+import kabopok.server.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import static kabopok.server.SampleObjectGenerator.createSampleProduct;
+import static kabopok.server.SampleObjectGenerator.createSampleUser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ProductTest extends AbstractTest {
+
+  @Autowired
+  private ProductRepository productRepository;
+  @Autowired
+  ProductMapper productMapper;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private HttpSteps httpSteps;
+
+  @BeforeEach
+  public void clear() {
+    productRepository.deleteAll();
+  }
+
+  @Test
+  @Order(1)
+  public void createProductTest() {
+    // Given
+    User user = createSampleUser();
+    userRepository.save(user);
+    Product product = createSampleProduct(user);
+    List<Resource> resources = List.of(
+            new ClassPathResource("images/photo1.jpg"),
+            new ClassPathResource("images/photo2.jpg")
+    );
+    // When
+    ProductDTO productDTO = productMapper.map(product);
+    productDTO.setUserID(user.getUserID());
+    String url = "/api/products/create";
+    ResponseEntity<Void> response = httpSteps.sendMultipartPostRequest(productDTO, resources, url, Void.class);
+    // Then
+    assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
+    List<Product> productList = productRepository.findAll();
+    productList.get(0).setUsersWishedBy(null);
+    productList.get(0).setPhotoUrl(product.getPhotoUrl());
+    productList.get(0).setUser(product.getUser());
+    productList.get(0).setProductID(product.getProductID());
+    assertEquals(productList.get(0), product);
+  }
+
+  @Test
+  @Order(2)
+  public void deleteProductTest() {
+    // Given
+    User user = createSampleUser();
+    userRepository.save(user);
+    Product product = createSampleProduct(user);
+    productRepository.save(product);
+    // When
+    ProductDTO productDTO = productMapper.map(product);
+    productDTO.setUserID(user.getUserID());
+    String url = "/api/products/delete?productId=" + product.getProductID();
+    ResponseEntity<Void> response = httpSteps.sendDeleteRequest(url);
+    // Then
+    assertEquals(200, response.getStatusCode().value());
+    assertFalse(productRepository.existsById(product.getProductID()));
+  }
+
+  @Test
+  @Order(3)
+  public void updateProductTest() {
+    // Given
+    User user = createSampleUser();
+    userRepository.save(user);
+    Product product = createSampleProduct(user);
+    productRepository.save(product);
+    product.setPrice("341");
+    // When
+    ProductDTO updatedProductDTO = productMapper.map(product);
+    updatedProductDTO.setUserID(user.getUserID());
+    List<Resource> updatedImages = List.of(
+            new ClassPathResource("images/photo2.jpg"),
+            new ClassPathResource("images/photo.jpg")
+    );
+    String url = "/api/products/update?productId=" + product.getProductID();
+    ResponseEntity<Void> response = httpSteps.sendMultipartPutRequest(updatedProductDTO, updatedImages, url, Void.class);
+    // Then
+    assertEquals(200, response.getStatusCode().value());
+    List<Product> productList = productRepository.findAll();
+    productList.get(0).setUsersWishedBy(null);
+    productList.get(0).setPhotoUrl(product.getPhotoUrl());
+    productList.get(0).setUser(product.getUser());
+    productList.get(0).setProductID(product.getProductID());
+    assertEquals(productList.get(0), product);
+  }
+
+  @Test
+  @Order(4)
+  public void getProductsTest() {
+    // Given
+    User user = createSampleUser();
+    userRepository.save(user);
+    for (int i = 0; i < 5; ++i) {
+      Product product = createSampleProduct(user);
+      productRepository.save(product);
+    }
+    // When
+    String url = "/api/products/get_products?page=1&limit=5";
+    ResponseEntity<List<ProductDTO>> response = httpSteps.sendGetRequest(url, new ParameterizedTypeReference<>() {});
+    // Then
+    assertEquals(HttpStatus.OK.value(), response.getStatusCode().value());
+    assertNotNull(response.getBody());
+    assertEquals(5, response.getBody().size());
+    List<Product> productList = productRepository.findAll();
+    List<ProductDTO> productListDTO = response.getBody();
+    for (int i = 0; i < 5; ++i) {
+      productList.get(i).setPhotoUrl(productListDTO.get(i).getPhotoUrl());
+      assertEquals(response.getBody().get(i), productMapper.map(productList.get(i)));
+    }
+  }
+
+  @Test
+  @Order(5)
+  public void getMyProductsTest() {
+    // Given
+    User user = createSampleUser();
+    userRepository.save(user);
+    for (int i = 1; i <= 5; ++i) {
+      productRepository.save(createSampleProduct(user));
+    }
+    User anotherUser = createSampleUser();
+    anotherUser.setUserName("anotherUser");
+    userRepository.save(anotherUser);
+    for (int i = 0; i < 4; ++i) {
+        productRepository.save(createSampleProduct(anotherUser));
+    }
+    // When
+    String url = "/api/products/get_my_products?page=1&limit=3&userId=" + anotherUser.getUserID();
+    ResponseEntity<List<ProductDTO>> response1 = httpSteps.sendGetRequest(url, new ParameterizedTypeReference<>() {});
+    url = "/api/products/get_my_products?page=2&limit=3&userId=" + anotherUser.getUserID();
+    ResponseEntity<List<ProductDTO>> response2 = httpSteps.sendGetRequest(url, new ParameterizedTypeReference<>() {});
+    // Then
+    assertEquals(HttpStatus.OK.value(), response1.getStatusCode().value());
+    assertEquals(HttpStatus.OK.value(), response2.getStatusCode().value());
+    assertNotNull(response1.getBody());
+    assertNotNull(response2.getBody());
+    assertEquals(3, response1.getBody().size());
+    assertEquals(1, response2.getBody().size());
+    Pageable pageable = PageRequest.of(0, 4);
+    List<Product> productList = productRepository.findAllByUser(anotherUser, pageable);
+    for (int i = 0; i < 3; ++i) {
+      productList.get(i).setPhotoUrl(response1.getBody().get(i).getPhotoUrl());
+      assertEquals(productMapper.map(productList.get(i)), response1.getBody().get(i));
+    }
+    productList.get(3).setPhotoUrl(response2.getBody().get(0).getPhotoUrl());
+    assertEquals(productMapper.map(productList.get(3)), response2.getBody().get(0));
+  }
+
+}
+

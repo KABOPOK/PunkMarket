541916979c1f8fb2bd09b8e21f1a96099483b267
KABOPOK <104580781+KABOPOK@users.noreply.github.com>
2024-12-13 22:44:24 +0300

    add cascade relations

--- server/src/main/resources/db/migrations/V3__add_wishlist_table.sql
+++ server/src/main/resources/db/migrations/V3__add_wishlist_table.sql
@@ -1,15 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS users (
-    user_id UUID PRIMARY KEY,
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     number TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     user_name TEXT NOT NULL,
     photo_url TEXT,
     location TEXT,
     telegram_id TEXT
-);
+    );
+
 CREATE TABLE IF NOT EXISTS products (
-    product_id UUID PRIMARY KEY,
+    product_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     price TEXT NOT NULL,
     title TEXT NOT NULL,
     owner_name TEXT NOT NULL,
@@ -19,23 +21,26 @@ CREATE TABLE IF NOT EXISTS products (
     category TEXT,
     user_id UUID NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-);
+    );
+
 CREATE TABLE IF NOT EXISTS chats (
-    chat_id UUID PRIMARY KEY,
+    chat_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     other_name TEXT NOT NULL,
     other_photo_url TEXT,
     other_chat_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    );
+
 CREATE TABLE IF NOT EXISTS messages (
-    message_id UUID PRIMARY KEY,
+    message_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     text TEXT NOT NULL,
     time TIMESTAMP NOT NULL,
     chat_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    );
+
 CREATE TABLE IF NOT EXISTS users_products (
     user_id UUID NOT NULL,
     product_id UUID NOT NULL,

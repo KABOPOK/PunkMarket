bb39bcb715211fbbf1bd37b00cec846490dfbb05
KABOPOK <mishavvvsweater@gmail.com>
2024-12-17 18:54:09 +0300

    fix localhost problem

--- server/src/main/java/kabopok/server/minio/MinioConfig.java
+++ server/src/main/java/kabopok/server/minio/MinioConfig.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Configuration
 public class MinioConfig {
   @Value("${minio.url}")
   private String minioUrl;
 
   @Bean
-  public MinioClient minioClient() {
+  public MinioClient minioClient() throws UnknownHostException {
+    InetAddress inetAddress = InetAddress.getLocalHost();
+    String ip = inetAddress.getHostAddress();
+    minioUrl = minioUrl.replace("localhost", ip);
     return MinioClient.builder()
             .endpoint(minioUrl)
             .credentials("minioadmin", "minioadmin")
